12506289f0cec3f686abfa6c376062b888a2907f
TtheBC01 <tac688@alumni.stanford.edu>
2022-09-14 17:19:32 +0000

    formatted cow.py

--- cow.py
+++ cow.py
@@ -1,35 +1,37 @@
-methanedensitystp = 0.0007168 # kg/L
-kgmethanepercowpermonth = 0.453592*10 # kg/(cow*month)
+# Constants and conversion factors
+methanedensitystp = 0.0007168 # kg/L at standard temperature and pressure
 joulesperkgofmethan = 50000000 # J/kg
 methanegeneratorefficiency = 0.36 # percent
 numberofsecondsinaday = 60*60*24 # seconds
 numberofhoursinamonth = 24*30 # hours
-numberofsecondsinamonth = 60*60*numberofhoursinamonth # seconds
 mathaneyieldperkg = 160 # - 216 L/kg
-kgofwasteperday = 85*0.453592 # kg/day
+kgofwasteperday = 133*0.453592 # kg/day
 
+# What is the methane production rate of a cow per day
 methanvolumepercowperday = kgofwasteperday*mathaneyieldperkg # L/(cow-day)
 kgofmethanepercowday = methanvolumepercowperday*methanedensitystp # kg/(cow-day)
-print("kg of methane per day:", kgofmethanepercowday)
+print(f'kg of methane per day: {kgofmethanepercowday} kg/day')
 
+# What is the power output of a cow from RNG
 cowenergyperday = kgofmethanepercowday*joulesperkgofmethan*methanegeneratorefficiency # J/day
 cowwatts = (cowenergyperday)/numberofsecondsinaday
+print(f'MJ of energy per day: {cowenergyperday/1000000} MJ/day')
+print(f'Cow Watts: {cowwatts} W/cow')
 
-print("MJ of energy per day:", cowenergyperday/1000000)
-print("Cow Watts:", cowwatts)
-
+# What are the per-block energy requiremnets of the Bitcoin network
 energypertransaction = 1720 # kWh/tx
 transactionsperblock = 1800 # txs
 mWhperblock = energypertransaction*transactionsperblock/1000 # mWh
+print(f'Bitcoin MW per block: {mWhperblock} BTC/MWh')
 
-print("Bitcoin MW per block:", mWhperblock)
-
+# What are the unit economics of the Bitcoin network
 blockreward = 6.25 # BTC/block
 priceperbitcoin = 20000 # USD
 dollarsperblock = blockreward*priceperbitcoin # USD/block
-
 dollarsperWh = dollarsperblock/(mWhperblock*1000000)
-print("$ per Wh:", dollarsperWh)
+print(f'$ per Wh: {dollarsperWh} $/Wh')
 
+# What can a cow expect to earn from RNG based electricity production from Bitcoin mining
 profitpercowpermonth = dollarsperWh*cowwatts*numberofhoursinamonth
-print("Cow earnings per month:", profitpercowpermonth)
+print(f'Cow earnings per month: ${profitpercowpermonth}')
+print(f'Cow earnings per year: ${profitpercowpermonth*12}')
